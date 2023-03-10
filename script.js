9141123bc573219592139939b73261a689a4ab14
Jmussleh <Jmussleh@gmail.comgit config --global user.name Jmussleh Jmussleh@gmail.com>
2023-03-09 20:43:41 -0500

    Added condition that if no characters were selected they would have to select one

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Assignment code here
-var SpecialC = "!#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
-var Numbers = "0123456789";
-var Lowercase = "abcdefghijklmnopqrstuvwxyz";
-var Uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var SpecialC = "!#$%&'()*+,-./:;<=>?@[\\]^_`{|}~".split('');
+var Numbers = "0123456789".split('');
+var Lowercase = "abcdefghijklmnopqrstuvwxyz".split('');
+var Uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,21 +10,17 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var Characters = window.prompt("How many characters would you like your password to contain? Choose between 8 and 128.");
-  if (isNaN(Characters) || passwordLength < 8 || passwordLength > 128) {
-    alert("Please enter a valid password length between 8 and 128 characters.");
-    return "";
-  }
   var SpecialC = window.confirm("Click OK to confirm including special characters. ")
   var Numbers = window.confirm("Click OK to confirm including numeric characters. ")
   var Lowercase = window.confirm("Click OK to confirm including lowercase characters. ")
   var Uppercase = window.confirm("Click OK to confirm including uppercase characters. ")
-
+  if (!Lowercase && !Uppercase && !Numbers && !SpecialC) {
+    alert("Please select at least one character set to include in your password.");
+    return "";
+  }
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
+  passwordText.value = password; }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
